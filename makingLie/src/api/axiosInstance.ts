0a12569c07dc54a6e873bf05533db9e9a2d7e2a0
최건희 <chlrjsgml21@cau.ac.kr>
2026-02-08 06:59:47 +0900

    vercel http오류 수정

--- makingLie/src/api/axiosInstance.ts
+++ makingLie/src/api/axiosInstance.ts
@@ -1,25 +1,28 @@
-// api/axiosInstance.ts
 import axios from "axios";
 import { getOrCreateCustomId } from "../utils/auth";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  // Vercel 배포 환경(PROD)이면 "/api" (vercel.json 프록시 타기)
+  // 로컬 환경이면 직접 백엔드 주소로 쏘기
+  baseURL: import.meta.env.PROD ? "/api" : "http://43.201.232.167:8000/api",
 });
 
 axiosInstance.interceptors.request.use((config) => {
   const id = getOrCreateCustomId();
 
+  // POST 요청 시 body에 id 주입
   if (config.method?.toLowerCase() === "post") {
     config.data = {
       ...config.data,
-      id, // ✅ 항상 같은 id
+      id,
     };
   }
 
+  // GET 요청 시 쿼리 스트링에 id 주입
   if (config.method?.toLowerCase() === "get") {
     config.params = {
       ...config.params,
-      id, // ✅ GET도 동일 id
+      id,
     };
   }
 
