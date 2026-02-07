20db93aa050d15f11b01f46343effa03be6271c2
최건희 <chlrjsgml21@cau.ac.kr>
2026-02-08 02:14:31 +0900

    에러페이지 생성

---
+++ makingLie/src/pages/ErrorPage.tsx
@@ -0,0 +1,10 @@
+import ErrorBox from "../components/ErrorBox";
+const ErrorPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <ErrorBox />
+    </div>
+  );
+};
+
+export default ErrorPage;
